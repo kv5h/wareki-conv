c932b483349720d6bffe533704de9976eb1c5a66
kv5h <107398371+kv5h@users.noreply.github.com>
2024-02-17 03:31:43 +0900

    4wsii56qmzl6gpojzayzuxikewysc7az

--- src/lib.rs
+++ src/lib.rs
@@ -34,4 +34,41 @@ mod tests {
             Utc.with_ymd_and_hms(2024, 2, 3, 0, 0, 0).unwrap()
         );
     }
+    fn test_jis_x0301_extended() {
+        let input_1 = "R01.02.03";
+        assert_eq!(
+            convert(input_1),
+            Utc.with_ymd_and_hms(2019, 2, 3, 0, 0, 0).unwrap()
+        );
+        let input_2 = "R10.2.3";
+        assert_eq!(
+            convert(input_2),
+            Utc.with_ymd_and_hms(2028, 2, 3, 0, 0, 0).unwrap()
+        );
+        let input_3 = "M01.02.03";
+        assert_eq!(
+            convert(input_3),
+            Utc.with_ymd_and_hms(1868, 2, 3, 0, 0, 0).unwrap()
+        );
+        let input_4 = "M45.2.3";
+        assert_eq!(
+            convert(input_4),
+            Utc.with_ymd_and_hms(1912, 2, 3, 0, 0, 0).unwrap()
+        );
+        let input_5 = "T01.02.03";
+        assert_eq!(
+            convert(input_5),
+            Utc.with_ymd_and_hms(1912, 2, 3, 0, 0, 0).unwrap()
+        );
+        let input_6 = "S01.2.3";
+        assert_eq!(
+            convert(input_6),
+            Utc.with_ymd_and_hms(1926, 2, 3, 0, 0, 0).unwrap()
+        );
+        let input_7 = "H01.02.03";
+        assert_eq!(
+            convert(input_7),
+            Utc.with_ymd_and_hms(1989, 2, 3, 0, 0, 0).unwrap()
+        );
+    }
 }
